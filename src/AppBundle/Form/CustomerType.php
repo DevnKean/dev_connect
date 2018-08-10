86a7cb7f6853024c8a993b8b532e7798889ff390
DevnKean <sokrafir@tutanota.com>
2018-08-10 06:14:16 +0200

    First commit

---
+++ src/AppBundle/Form/CustomerType.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Customer;
+use AppBundle\Entity\Location;
+use AppBundle\Entity\Service;
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PercentType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class CustomerType extends AbstractType
+{
+    /**
+     * @var User
+     */
+    private $user;
+
+    public function __construct(TokenStorageInterface $storage)
+    {
+        $this->user = $storage->getToken()->getUser();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $supplier = $this->user->getSupplier();
+        $builder
+            ->add('titleC', TextType::class, [
+                'label' => 'CCCC',
+                'required' => false,
+                'attr' => [                    
+                    'placeholder' => 'Optional: Please enter the customer company name',
+                ],              
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Name',
+                'required' => false,
+                'attr' => [                    
+                    'placeholder' => 'Optional: Please enter the customer company name',
+                ],
+            ])
+            ->add('industryVertical', ChoiceType::class, [
+                'label' => 'Industry vertical',
+                'choices' => array_combine(Customer::getIndustryVerticals(), Customer::getIndustryVerticals()),
+                'placeholder' => 'Please select industry vertical',
+                'attr' => [
+                    'class' => 'select2',
+                    'data-placeholder' => 'Please select industry vertical',
+                ],
+            ])
+            ->add('functions', ChoiceType::class, [
+                'choices' => array_combine(Service::getFunctions(), Service::getFunctions()),
+                'label' => 'Functions (choose all functions that apply to this customer)',
+                // 'placeholder' => 'Please select the service(s) you supply for this customer',
+                'multiple' => true,
+                'attr' => [
+                    'class' => 'select2',
+                    'data-placeholder' => 'Please select the service(s) you supply for this customer',
+                ],
+            ]);
+        if ($supplier->isOutSourcing()) {
+            $builder
+                ->add('totalSeats', IntegerType::class, [
+                    'label' => 'Total seats',
+                    
+                ])
+                ->add('percentageOfBusiness', PercentType::class, [
+                    'label' => 'Percentage of business',
+                    'scale' => 2,
+                ]);
+        }
+
+        $builder
+            ->add('locations', EntityType::class, [
+                'class' => Location::class,
+                'choice_label' => 'address',
+                'label' => 'Locations',
+                'query_builder' => function (EntityRepository $er) use ($options) {
+                    return $er->createQueryBuilder('l')
+                              ->where('l.supplier = :supplier')
+                              ->orderBy('l.id', 'ASC')
+                              ->setParameter('supplier', $options['supplier']);
+                },
+                'multiple' => true,
+                'placeholder' => 'Please select which locations this customer is managed from',
+                'attr' => [
+                    'class' => 'select2',
+                    'data-placeholder' => 'Please select which locations this customer is managed from',
+                ],
+            ])
+            ->add('workPeriod', ChoiceType::class, [
+                'label' => 'How long have you had the work?',
+                'choices' => array_combine(Customer::getYears(), Customer::getYears()),
+                'placeholder' => 'Please select the work period',
+                'attr' => [
+                    'class' => 'select2',
+                    'data-placeholder' => 'Please select the work period',
+                ],
+            ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Customer',
+        ])
+                 ->setRequired('supplier');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'CustomerType';
+    }
+
+
+}
